48cabe2a634714c3a22f33fb3bfabfcefbeb44b1
Lu <rubertolh07@gmail.com>
2025-04-22 18:29:39 +0000

    almost done practica2.py

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import sys
-import os
-
 from src.practica1 import *
 from src.practica2 import *
 from src.practica3 import *
@@ -11,6 +8,7 @@ def main():
     grafo_lista = lee_grafo_stdin(grafo)
     imprime_grafo_lista(grafo_lista)
     print("Grados de los vértices:", cuenta_grado(grafo_lista))
+    print("Vértices aislados:", vertice_aislado(grafo_lista))
 
 if __name__ == '__main__':
     main()
--- src/practica2.py
+++ src/practica2.py
@@ -1,5 +1,5 @@
 def cuenta_grado(grafo_lista):
-    '''
+    ''' DONE
     Muestra por pantalla los grados de los vertices de un grafo. 
     El argumento esta en formato de lista.
     
@@ -19,14 +19,22 @@ def cuenta_grado(grafo_lista):
     return grados
 
 def vertice_aislado(grafo_lista):
-    '''
+    ''' DONE
     Dado un grafo en representacion de lista, obtiene una lista de los vértices aislado.
     Ejemplo Entrada: 
         (['A','B','C','D','E'],[('A','B'),('B','C'),('C','B')])
     Ejemplo formato salida: 
         ['D','E']
     '''
-    pass
+    output = []
+    vertices = grafo_lista[0]
+    grados = cuenta_grado(grafo_lista)
+
+    for vertice in vertices:
+        if grados[vertice] == 0:
+            output.append(vertice)
+
+    return output
 
 def componentes_conexas(grafo_lista):
     '''
@@ -43,4 +51,4 @@ def es_conexo(grafo_lista):
     Dado un grafo en representacion de lista, y utilizando la función "componentes_conexas"
     devuelve True/False si el grafo es o no conexo.
     '''
-    pass
+    return len(componentes_conexas(grafo_lista)) == 1
